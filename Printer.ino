91c9c8f09da1499a8b49ae942f376857732e0e46
Arduino Enigma <arduinoenigma@gmail.com>
2017-08-27 17:48:15 -0400

    Update Printer.ino

--- Printer.ino
+++ Printer.ino
@@ -1,7 +1,8 @@
-byte PrintMaxLine = 8;
+byte PrintMaxLine = 16;
 byte PrintMaxGroup = 4;
 byte PrintGroup = 0;
 byte PrintLine = 0;
+byte PrintedGroups = 0;
 
 void InitPrinter()
 {
@@ -13,21 +14,25 @@ void SetPrinterGroups(byte groups)
 {
   PrintMaxGroup = groups;
 
-  Serial.print(F("[PRINTER:"));
-  Serial.print(PrintMaxGroup);
-  Serial.println(F("]"));
+  //Serial.print(F("[PRINTER:"));
+  //Serial.print(PrintMaxGroup);
+  //Serial.println(F("]"));
 }
 
 void PrinterFeed()
 {
-  Serial.println(F("[PRINTER FEED]"));
+  //Serial.println(F("[PRINTER FEED]"));
   PrintGroup = 0;
   PrintLine = 0;
+  PrintedGroups = 0;
+
+  printer.feed(2);
 }
 
 void Print(char c)
 {
-  Serial.print(c);
+  //Serial.print(c);
+  printer.print(c);
 
   PrintGroup++;
   PrintLine++;
@@ -38,9 +43,14 @@ void Print(char c)
   }
   else if (PrintGroup == PrintMaxGroup)
   {
-    Serial.print(F(" "));
+    //Serial.print(F(" "));
+    printer.print(F(" "));
     PrintGroup = 0;
+    PrintedGroups++;
   }
 
+  if (((PrintedGroups == 2) && ((PrintMaxGroup == 5) || (PrintMaxGroup == 6))) || ((PrintedGroups == 3) && (PrintMaxGroup == 4)))
+  {
+    PrinterFeed();
+  }
 }
-
